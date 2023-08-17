65888cb97d254c3df0a269e6b1a243c75d09b0b3
EC2 Default User <ec2-user@ip-172-31-40-174.ap-northeast-1.compute.internal>
2023-08-17 13:08:20 +0000

    kadai3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,37 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def show
+     @book = Book.new
+     @books = Book.all
+     @users = User.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to user_paths
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @user = current_user
   end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to user_path(@user.id)
+    else
+    render :edit
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+  
 end
